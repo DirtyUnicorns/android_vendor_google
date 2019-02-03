2d5a16ce0dda4d61259e592f5fd1b3406215ec41
DeviousFusion <dj3tusk@gmail.com>
2019-02-03 04:53:16 -0600

    marlin: Refactor vendor tree
    Change-Id: I4ab8830a144981d7468dd9af6ce0b3f8b229da03

--- marlin/BoardConfigVendor.mk
+++
@@ -1,7 +0,0 @@
-AB_OTA_PARTITIONS += vendor
-ifneq ($(filter sailfish,$(TARGET_DEVICE)),)
-  LOCAL_STEM := sailfish/BoardConfigVendorPartial.mk
-else
-  LOCAL_STEM := marlin/BoardConfigVendorPartial.mk
-endif
--include vendor/google/$(LOCAL_STEM)
